0cf52fd912e9ee1d37b091ad9b28d5aa89d0b9d3
腾宇Matthew <31982122+ChineseGardenCat@users.noreply.github.com>
2019-03-08 09:20:41 +1100

    Improved crawler and add Firebase methods

--- chemist/chemist/pipelines.py
+++ chemist/chemist/pipelines.py
@@ -5,7 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from firebase import firebase
+import json
+import requests
+
+firebase = firebase.FirebaseApplication('https://chemist-helper.firebaseio.com')
 
 class ChemistPipeline(object):
+
+
     def process_item(self, item, spider):
+
+        data = {'image': item['image_url'], 'product_name': item['name'], 'product_price': item['price'].strip(),
+                'shop_url': item['shop_url']}
+        sent = json.dumps(data)
+        print(sent)
+        result = requests.put("https://chemist-helper.firebaseio.com/products/" + item['name']+".json", data = sent)
+        print(result)
         return item
--- chemist/chemist/spiders/chemist_spider.py
+++ chemist/chemist/spiders/chemist_spider.py
@@ -27,17 +27,13 @@ class ChemistSpider(scrapy.Spider):
         for suffix in temp:
             url = self.chemist_prefix + suffix
             yield scrapy.Request(url, callback=self.parse_product)
+        banner = soup.find_all('a', href=re.compile(r"^https://www.chemistwarehouse.com.au/shop-online/*"))
+        for category in banner:
+            url = category.get('href')
+            yield scrapy.Request(url, callback=self.parse_product)
 
     def parse_product(self, response):
         soup = BeautifulSoup(response.body, 'html.parser')
-        next_tags = set(soup.find_all('a', string='Next'))
-        next_arr = []
-        for ele in next_tags:
-            next_arr.append(ele.get('href'))
-        if next_arr:
-            for suffix in next_arr:
-                url = self.chemist_prefix + suffix
-                yield scrapy.Request(url, callback=self.parse_product)
         products = soup.find_all('a', class_="product-container")
         new_item = ChemistItem()
         for product in products:
@@ -47,4 +43,12 @@ class ChemistSpider(scrapy.Spider):
             new_item['price'] = product.find('span', class_='Price').text
             new_item['image_url'] = product.find('img').get('src')
             yield new_item
+        next_tags = set(soup.find_all('a', string='Next'))
+        next_arr = []
+        for ele in next_tags:
+            next_arr.append(ele.get('href'))
+        if next_arr:
+            for suffix in next_arr:
+                url = self.chemist_prefix + suffix
+                yield scrapy.Request(url, callback=self.parse_product)
 
